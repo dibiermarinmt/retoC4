d68c023b3a0f433b756a6877f6c9e409a0274214
Dibier <dibier.marin.mt@correo.usa.edu.co>
2021-11-29 14:13:37 -0500

    Ajustes cross origin en javaApplication.

--- src/main/java/com/retoC4/RetoC4Application.java
+++ src/main/java/com/retoC4/RetoC4Application.java
@@ -3,12 +3,13 @@ package com.retoC4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EntityScan(basePackages = ("com.retoC4.model"))
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RetoC4Application {
 
 	public static void main(String[] args) {
