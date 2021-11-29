33f3d6df685b06ac45e6f20ed146f609e283a2de
Dibier <dibier.marin.mt@correo.usa.edu.co>
2021-11-29 13:14:43 -0500

    Ajustes cross origin.

--- src/main/java/com/retoC4/RetoC4Application.java
+++ src/main/java/com/retoC4/RetoC4Application.java
@@ -2,7 +2,9 @@ package com.retoC4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = ("com.retoC4.model"))
 @SpringBootApplication
 public class RetoC4Application {
 
