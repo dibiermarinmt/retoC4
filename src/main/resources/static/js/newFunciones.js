318101c790d77ce4e9900994c2870a49bcb6eb10
Tu Nombre <you@example.com>
2021-11-28 20:40:06 -0500

    arreglados campos de nuevo usuario

--- src/main/resources/static/js/newFunciones.js
+++ src/main/resources/static/js/newFunciones.js
@@ -1,4 +1,5 @@
 let raiz = "http://localhost:8080/api/user";
+let alerta = "";
 
 function registrarUser(){
     let user = {
@@ -19,16 +20,73 @@ function registrarUser(){
     });
 }
 
+function nombreValido() {
+    if($("#nombre").val() != "") {
+        return true;
+    } else {
+        alerta ="Nombre no valido.";
+        return false;
+    }
+}
+
+
+function existeCorreo(correo) {
+    let existe = false;
+    $.ajax({
+        url: raiz + "/" + correo,
+        type: "GET",
+        async: false,
+        dataType: "JSON",
+        success: function(respuesta) {
+            existe = respuesta;
+        }
+    });
+    return existe;
+}
+
+function correoValido() {
+    let esCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test($("#correo").val());
+    
+    if(!esCorreo) {
+        alerta = "No es un correo electrónico valido."
+        return false;
+    } else if(existeCorreo($("#correo").val())) {
+        alerta = "Este correo ya está registrado.";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function claveValida() {
+    if($("#clave").val() == "") {
+        alerta = "Contraseña vacía. Escriba una contraseña";
+        return false;
+    } else if($("#clave2").val() == "") {
+        alerta = "Reescriba la contraseña en el segundo campo.";
+    } else if($("#clave").val() != $("#clave2").val()) {
+        alerta = "Contraseñas diferentes.";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 function datosValidos() {
-    //let esCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test($("#correo").val());
-    let clavesIguales = ($("#clave").val() == $("#clave2").val());
-    return clavesIguales;
+    let validos = true;
+    validos = validos && nombreValido();
+    validos = validos && correoValido();
+    validos = validos && claveValida();
+    return validos;
 }
 
 function crearUser() {
     if(datosValidos()) {
         registrarUser();
     } else {
-        alert("Usuario no creado");
+        alert(alerta);
+        alerta = "";
     }
+    //console.log(existeCorreo($("#correo").val()));
 }
